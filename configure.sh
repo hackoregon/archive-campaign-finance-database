19fb190f9f3f69aada119df8a3f72f9aa4534514
James Darabi <mail@jdarabi.com>
2015-03-26 14:09:30 -0700

    Updated scraper.sh

---
+++ configure.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo pip install csvkit
+
+mkdir -p scraped_data/comms
+mkdir -p scraped_data/fins
--- scraper/scraper.sh
+++ scraper/scraper.sh
@@ -1,26 +1,33 @@
-curl -s -c cookies -o /dev/null "https://secure.sos.state.or.us/orestar/GotoSearchByElection.do"
+#!/bin/bash
+
+shopt -s expand_aliases
+alias curl="curl -s -A \"Mozilla/5.0\""
+
+curl -c cookies -o /dev/null "https://secure.sos.state.or.us/orestar/GotoSearchByElection.do"
 
 search_page="https://secure.sos.state.or.us/orestar/CommitteeSearchSecondPage.do"
 dump_page="https://secure.sos.state.or.us/orestar/XcelSooSearch"
 
-for year in $(seq 2016 -1 1989)
+for year in $(seq 2016 -1 2015)
 do
     if [ ! -f scraped_data/comms/$year.xls ]; then
+        #  committees
         echo "searching $year"
-        curl -s -b cookies -o /dev/null --data "yearActive=$year&discontinuedSOO=on&buttonName=electionSearch" $search_page
+        curl -b cookies -o /dev/null --data "yearActive=$year&discontinuedSOO=true&buttonName=electionSearch" $search_page
         echo "downloading $year"
-#    curl -s -b cookies 'https://secure.sos.state.or.us/orestar/XcelSooSearch' | in2csv -f xls > scraped_data/comms/$year.csv
-        curl -s -b cookies $dump_page > scraped_data/comms/$year.xls
+        curl -b cookies $dump_page > scraped_data/comms/$year.xls
 
+        # candidate controlled committees
         echo "searching $year"
-        curl -s -b cookies -o /dev/null --data "yearActive=$year&allCandidateConComm=on&buttonName=electionSearch" $search_page
+        curl -b cookies -o /dev/null --data "yearActive=$year&allCandidateConComm=true&buttonName=electionSearch" $search_page
         echo "downloading $year"
-        curl -s -b cookies $dump_page > scraped_data/comms/$year-cc.xls
+        curl -b cookies $dump_page > scraped_data/comms/$year-cc.xls
 
+        # slate mailer organizations
         echo "searching $year"
-        curl -s -b cookies -o /dev/null --data "yearActive=$year&allSlateMailer=on&buttonName=electionSearch" $search_page
+        curl -b cookies -o /dev/null --data "yearActive=$year&allSlateMailer=true&buttonName=electionSearch" $search_page
         echo "downloading $year"
-        curl -s -b cookies $dump_page > scraped_data/comms/$year-sl.xls
+        curl -b cookies $dump_page > scraped_data/comms/$year-sl.xls
     else
         echo "skipping $year"
     fi
@@ -36,11 +43,11 @@ do
             last_tran=$(date +%D)
             while true; do
                 echo "searching $comm_id from $last_tran"
-                curl -s -b cookies -o /dev/null "https://secure.sos.state.or.us/orestar/gotoPublicTransactionSearchResults.do?cneSearchFilerCommitteeId=$comm_id&cneSearchTranEndDate=$last_tran"
+                curl -b cookies -o /dev/null "https://secure.sos.state.or.us/orestar/gotoPublicTransactionSearchResults.do?cneSearchFilerCommitteeId=$comm_id&cneSearchTranEndDate=$last_tran"
                 echo "downloading $comm_id"
-                curl -s -b cookies 'https://secure.sos.state.or.us/orestar/XcelCNESearch' > scraped_data/fins/$comm_id-$counter.xls
+                curl -b cookies "https://secure.sos.state.or.us/orestar/XcelCNESearch" > scraped_data/fins/$comm_id-$counter.xls
                 if [ $(in2csv scraped_data/fins/$comm_id-$counter.xls | wc -l) = 5000 ]; then
-                    last_tran=$(in2csv scraped_data/fins/$comm_id-$counter.xls | csvcut -c "Tran Date" | tail -n 1)
+                    last_tran=$(in2csv scraped_data/fins/$comm_id-$counter.xls | csvcut -e latin1 -c "Tran Date" | tail -n 1)
                     let counter=counter+1
                 else
                     break
